bb9564d23f8368dba6f5a880b31b90cd2ead8f24
Richard Wattenbarger <github@wattenbarger.net>
2023-04-12 21:44:55 -0400

    remove Csv capabilities (needs to be refactored)

--- startup.py
+++ startup.py
@@ -3,24 +3,11 @@ import io
 
 from functools import singledispatch
 from itertools import chain
+from math import floor, log10
 from pathlib import Path
 from typing import Protocol, runtime_checkable
 
 _BOM = b'\xef\xbb\xbf'
-
-@runtime_checkable
-class CsvReaderFactoryProtocol(Protocol):
-    def create(fp, *args, **kwargs) -> csv.reader:
-        ...
-
-class CsvReaderFactory():
-    def create(fp, *args, **kwargs) -> csv.reader:
-        return csv.reader(fp, *args, **kwargs)
-    
-class CsvDictReaderFactory():
-    def create(fp, fieldnames, *args, **kwargs) -> csv.reader:
-        return csv.DictReader(fp, fieldnames, *args, **kwargs)
-
 
 def get_all_files(parent: Path=Path.cwd()):
     return chain((p for p in parent.iterdir()
@@ -59,30 +46,12 @@ def supports_excel(p: Path):
 def _(filename: str):
     return supports_excel(Path(filename))
 
-@singledispatch
-def read_csv(p: Path, reader_factory: CsvReaderFactoryProtocol, 
-             dialect='excel', transform=tuple, fieldnames=None):
-    
-    encoding = 'utf-8-sig' if supports_excel(p) else 'utf-8'
-    with p.open(encoding=encoding) as fp:
-        return [transform(row) for row 
-                in reader_factory.create(fp, dialect=dialect)]
-
-@read_csv.register
-def _(filename: str, reader_type=csv.reader, dialect='excel', transform=tuple):
-    return read_csv(Path(filename), reader_type=reader_type, dialect=dialect,
-                    transform=transform)
-
-@singledispatch
-def write_csv(p: Path, rows, writer_type=csv.writer, supports_excel=False):
-    encoding = 'utf-8-sig' if supports_excel else 'utf-8'
-    with p.open(mode='w') as fp:
-        writer = writer_type(fp)
-        try:
-            writer.writeheader()
-        except AttributeError:
-            ...
-        writer.writerows(rows)
+# TODO: implement line wrapping, scroll control
+def print_numbered(lines: list[str]) -> None:
+    number_width = floor(log10(len(lines))) + 2
+    format_string = f'{{0:>{number_width}}} {{1}}'
+    for i, line in enumerate(lines):
+        print(format_string.format(i, line))
 
 def _test():
     ...
